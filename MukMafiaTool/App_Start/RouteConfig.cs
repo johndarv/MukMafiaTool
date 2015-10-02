25942bc4666531fe66d061bcc1172d92cf00c8cf
johndarv <johndarv@hotmail.com>
2015-10-02 13:11:10 +0100

    First attempts at making Players editable.

--- MukMafiaTool/App_Start/RouteConfig.cs
+++ MukMafiaTool/App_Start/RouteConfig.cs
@@ -18,6 +18,12 @@ namespace MukMafiaTool
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+
+            routes.MapRoute(
+                name: "Login",
+                url: "login",
+                defaults: new { controller = "Authentication", action = "Index" }
+            );
         }
     }
 }
---
+++ MukMafiaTool/Controllers/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MukMafiaTool.Common;
+
+namespace MukMafiaTool.Controllers
+{
+    public class PlayerController : Controller
+    {
+        private IRepository _repo;
+
+        public PlayerController(IRepository repo)
+        {
+            _repo = repo;
+        }
+
+        // GET: Player
+        [Authorize(Roles = "Admin")]
+        public ActionResult Edit(string playerName)
+        {
+            if (string.IsNullOrEmpty(playerName))
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            var player = _repo.FindPlayer(playerName);
+
+            if (player == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(player);
+        }
+    }
+}
