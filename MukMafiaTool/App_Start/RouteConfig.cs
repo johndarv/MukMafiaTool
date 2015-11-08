b13b8ec0ad7b62a9648555b9af076fd115c2adec
JohnDarv <JohnDarv@Hotmail.com>
2015-11-08 12:31:03 +0000

    Fixing the slash login url route.

--- MukMafiaTool/App_Start/RouteConfig.cs
+++ MukMafiaTool/App_Start/RouteConfig.cs
@@ -14,15 +14,15 @@ namespace MukMafiaTool
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
-
-            routes.MapRoute(
                 name: "Login",
                 url: "login",
                 defaults: new { controller = "Authentication", action = "Index" }
+            );
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
