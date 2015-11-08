c83a04a94849396ee0815f88827a26fa0b16e5df
JohnDarv <JohnDarv@Hotmail.com>
2015-11-08 19:02:50 +0000

    Enhancing log message viewing.

--- MukMafiaTool/Controllers/LogController.cs
+++ MukMafiaTool/Controllers/LogController.cs
@@ -1,7 +1,9 @@
 ﻿using MukMafiaTool.Common;
+using MukMafiaTool.Models.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Web;
 using System.Web.Mvc;
 
@@ -20,7 +22,30 @@ namespace MukMafiaTool.Controllers
         [Authorize(Roles = "Admin")]
         public ActionResult Index()
         {
-            return View(_repo.FindAllLogMessages());
+            var messages = _repo.FindAllLogMessages();
+            IList<LogMessage> logMessages = new List<LogMessage>();
+
+            var postNumberRegex = new Regex("Forum Post Number: [0-9]+");
+            var numbersRegex = new Regex("[0-9]+");
+
+            foreach (var message in messages)
+            {
+                var logMessage = new LogMessage();
+                logMessage.Message = message;
+
+                var postNumberMatch = postNumberRegex.Match(message);
+
+                if (postNumberMatch.Success)
+                {
+                    var numbersMatch = numbersRegex.Match(postNumberMatch.Value);
+
+                    logMessage.ForumPostNumber = numbersMatch.Value;
+                }
+
+                logMessages.Add(logMessage);
+            }
+
+            return View(logMessages);
         }
     }
 }
---
+++ MukMafiaTool/Models/ViewModels/LogMessage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MukMafiaTool.Models.ViewModels
+{
+    public class LogMessage
+    {
+        public string Message { get; set; }
+        public string ForumPostNumber { get; set; }
+    }
+}
