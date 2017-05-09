dc44ba0677ee35f759af61b8ddfaef64e7c28345
John Darvill <john.darvill@experian.com>
2017-05-09 20:38:56 +0100

    Code analysis warning fixes.

--- MukMafiaTool/App_Start/WebApiConfig.cs
+++ MukMafiaTool/App_Start/WebApiConfig.cs
@@ -1,10 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web.Http;
-
-namespace MukMafiaTool
+﻿namespace MukMafiaTool
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web.Http;
+
     public static class WebApiConfig
     {
         public static void Register(HttpConfiguration config)
@@ -14,8 +14,7 @@ namespace MukMafiaTool
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
-                defaults: new { id = RouteParameter.Optional }
-            );
+                defaults: new { id = RouteParameter.Optional });
         }
     }
 }
