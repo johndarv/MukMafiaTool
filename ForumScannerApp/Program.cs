025e365c289bcadbbf6dc7b3f7e1038a88d01de9
John Darvill <john.darvill@experian.com>
2017-05-19 12:26:48 +0100

    Allowing the ForumScannerApp to keeping scanning every x minutes.

--- ForumScannerApp/Program.cs
+++ ForumScannerApp/Program.cs
@@ -1,9 +1,13 @@
 ﻿namespace ForumScannerApp
 {
     using System;
+    using System.Configuration;
     using System.Globalization;
     using System.Linq;
+    using System.Threading;
+    using System.Timers;
     using log4net;
+    using MukMafiaTool.Common;
     using MukMafiaTool.Database;
     using MukMafiaTool.ForumScanning;
 
@@ -15,26 +19,53 @@
         {
             log4net.Config.XmlConfigurator.Configure();
 
-            if (args.Any() && string.Equals(args.First(), "doScan", StringComparison.OrdinalIgnoreCase))
+            using (var repository = new MongoRepository())
             {
-                using (var repository = new MongoRepository())
+                if (args.Any() && string.Equals(args.First(), "doScan", StringComparison.OrdinalIgnoreCase))
                 {
-                    try
+                    DoScan(repository);
+                }
+                else
+                {
+                    var interval = TimeSpan.FromMinutes(5);
+                    var intervalInConfig = ConfigurationManager.AppSettings["ScanIntervalInMinutes"];
+                    int intervalAsInteger = 0;
+
+                    if (int.TryParse(intervalInConfig, out intervalAsInteger))
                     {
-                        var scanner = new ForumScanner(repository);
-
-                        scanner.DoWholeUpdate();
-
-                        Log.Info("Scan complete!");
+                        interval = TimeSpan.FromMinutes(intervalAsInteger);
                     }
-                    catch (Exception exception)
+
+                    var timer = new System.Timers.Timer(interval.TotalMilliseconds);
+                    timer.Enabled = true;
+                    timer.Elapsed += new ElapsedEventHandler((obj, eventHandler) => DoScan(repository));
+
+                    using (var resetEvent = new ManualResetEventSlim(initialState: false))
                     {
-                        var message = string.Format(CultureInfo.InvariantCulture, "Exception thrown when attempting to update repository from the forum: {0}", exception.Message);
-
-                        Log.Error(message);
-                        repository.LogMessage(message);
+                        Console.CancelKeyPress += (s, e) => resetEvent.Set();
+                        timer.Start();
+                        resetEvent.Wait();
                     }
-                }                
+                }
+            }
+        }
+
+        internal static void DoScan(IRepository repository)
+        {
+            try
+            {
+                var scanner = new ForumScanner(repository);
+
+                scanner.DoWholeUpdate();
+
+                Log.Info("Scan complete!");
+            }
+            catch (Exception exception)
+            {
+                var message = string.Format(CultureInfo.InvariantCulture, "Exception thrown when attempting to update repository from the forum: {0}", exception.Message);
+
+                Log.Error(message);
+                repository.LogMessage(message);
             }
         }
     }
